cff8376606f20eabe314d022a08a7bdce42ee2f9
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2019-10-02 13:25:25 -0400

    initial commit

---
+++ comment.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import json
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+
+def click(element):
+  driver.execute_script("arguments[0].click()", element)
+
+def run(driver):
+  driver.implicitly_wait(2) # seconds
+  comment_js = open("comments.js").read()
+
+  for slug in sys.argv[1:]:
+    fname = "output/%s.raw.json" % slug
+    if os.path.exists(fname):
+      continue
+
+    print(slug)
+    driver.get("https://www.facebook.com/jefftk/posts/" + slug)
+    try:
+      see_comments = driver.find_element_by_css_selector(
+          "a[data-testid='UFI2CommentsCount/root']")
+    except NoSuchElementException:
+      result = []
+    else:
+      click(see_comments)
+      driver.execute_script(comment_js)
+      WebDriverWait(driver, 15).until(EC.title_is("ready"))
+    
+      result = driver.execute_script("return window.COLLECTED_COMMENTS;");
+
+    with open(fname, "w") as outf:
+      outf.write(json.dumps(result))
+
+    time.sleep(5)
+
+if __name__ == "__main__":
+  try:
+    driver = webdriver.Chrome()
+    run(driver)
+  finally:
+    driver.quit()
+    pass
+
+
+
+
---
+++ comments.js
@@ -0,0 +1,70 @@
+var expand_selectors = 
+  // expand_comments
+  "a[data-testid='UFI2CommentsPagerRenderer/pager_depth_0']," +
+  // expand_replies
+  "a[data-testid='UFI2CommentsPagerRenderer/pager_depth_1']," +
+  // see more
+  "div[data-testid='UFI2Comment/body'] a[role='button']";
+
+var close_cta = "a[id='expanding_cta_close_button']";
+
+// As long as there are things to click on, click on them.  If there's been
+// nothing to click on for 1s decide we're done.
+var last_click = Date.now();
+function expand_things(callback) {
+  for (var clickable of document.querySelectorAll(expand_selectors)) {
+    clickable.click();
+    last_click = Date.now();
+  }
+  if (last_click > (Date.now() - 1000)) {
+    window.setTimeout(function() {
+      expand_things(callback);
+    }, 100);
+  } else {
+    if (callback) {
+      callback();
+    }
+  }
+}
+
+function process_comment(comment_div) {
+  var user_id = comment_div.querySelector(
+       "a[data-hovercard^='/ajax/hovercard/user.php?id=']").getAttribute(
+       "data-hovercard").replace("/ajax/hovercard/user.php?id=", "");
+  var body_div = comment_div.querySelector(
+       "div[data-testid='UFI2Comment/body'] > div");
+
+  try {
+    var name = body_div.children[0].textContent;
+    var comment_html = body_div.children[1].innerHTML;
+    return [name, user_id, comment_html];
+  } catch(e) {
+    return null;
+  }
+}
+
+function examine_comments() {
+  collected_comments = [];
+  for (var top_level_li of document.querySelectorAll(
+          "div[data-testid='UFI2CommentsList/root_depth_0'] > ul > li")) {
+    parent_comment = process_comment(top_level_li.querySelector(
+        "div[aria-label='Comment']"));
+    child_comments = [];
+    for (var child_comment_div of top_level_li.querySelectorAll(
+            "div[aria-label='Comment reply']")) {
+      child_comments.push(process_comment(child_comment_div));
+    }
+    collected_comments.push([parent_comment, child_comments]);
+  }
+  window.COLLECTED_COMMENTS = collected_comments;
+  document.title = "ready";
+}
+
+expand_things(function() {
+  var cta_button = document.querySelector(close_cta);
+  if (cta_button) {
+    cta_button.click();
+  }
+  examine_comments();
+});
+
