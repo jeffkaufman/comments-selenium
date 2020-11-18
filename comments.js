26b927155b017a869d2d940f4db039c5952f8887
Jeff Kaufman <jeff@jefftk.com>
2020-11-18 10:32:47 -0500

    revive comments.js

--- comments.js
+++ comments.js
@@ -1,10 +1,11 @@
-var expand_selectors =
-  // expand_comments
-  "a[data-testid='UFI2CommentsPagerRenderer/pager_depth_0']," +
-  // expand_replies
-  "a[data-testid='UFI2CommentsPagerRenderer/pager_depth_1']," +
-  // see more
-  "div[data-testid='UFI2Comment/body'] a[role='button']";
+var open_comments_class_name = "_3hg- _42ft";
+var view_more_class_name = "_4ssp";
+var see_more_class_name = "_5v47";
+var top_level_comments_class_name = "_7a9a";
+var reply_comments_class_name = "_7a9h";
+var comment_class_name = "_3l3x";
+var author_class_name = "_6qw4";
+var metadata_class_name = "_6qw7";
 
 var close_cta = "a[id='expanding_cta_close_button']";
 
@@ -12,10 +13,17 @@ var close_cta = "a[id='expanding_cta_close_button']";
 // nothing to click on for 1s decide we're done.
 var last_click = Date.now();
 function expand_things(callback) {
-  for (var clickable of document.querySelectorAll(expand_selectors)) {
-    clickable.click();
+  function click(x) {
+    x.click();
     last_click = Date.now();
   }
+  for (var x of document.getElementsByClassName(view_more_class_name)) {
+    click(x);
+  }
+  for (var x of document.getElementsByClassName(see_more_class_name)) {
+    click(x);
+  }
+
   if (last_click > (Date.now() - 1000)) {
     window.setTimeout(function() {
       expand_things(callback);
@@ -27,49 +35,67 @@ function expand_things(callback) {
   }
 }
 
-function process_comment(comment_div) {
-  var data_hovercard = comment_div.querySelector(
-     "a[data-hovercard^='/ajax/hovercard/user.php?id=']");
-  var user_id = data_hovercard ? data_hovercard.getAttribute(
-     "data-hovercard").replace("/ajax/hovercard/user.php?id=", "") : "-1";
-  var body_div = comment_div.querySelector(
-       "div[data-testid='UFI2Comment/body'] > div");
-  var actions = comment_div.querySelector(
-       "ul[data-testid='UFI2CommentActionLinks/root'] > li >" +
-       "a[href^='https://www.facebook.com/jefftk/posts/']");
-  if (!actions) {
-    actions = comment_div.querySelector(
-       "ul[data-testid='UFI2CommentActionLinks/root'] > li >" +
-       "a[href^='https://www.facebook.com/photo.php']");
-  }
-  if (!actions) {
-    actions = comment_div.querySelector(
-       "ul[data-testid='UFI2CommentActionLinks/root'] > li >" +
-       "a[href^='https://www.facebook.com/notes/jeff-kaufman/']");
-  }
-  var abbr = actions.querySelector("abbr[data-utime]");
-
-  try {
-    var name = body_div.children[0].textContent;
-    var comment_html = body_div.children[1].innerHTML;
-    var ts = abbr.getAttribute("data-utime");
-    var link = actions.getAttribute("href");
-    return [name, link, user_id, ts, comment_html];
-  } catch(e) {
-    return null;
-  }
-}
-
 function examine_comments() {
   collected_comments = [];
-  for (var top_level_li of document.querySelectorAll(
-          "div[data-testid='UFI2CommentsList/root_depth_0'] > ul > li")) {
-    parent_comment = process_comment(top_level_li.querySelector(
-        "div[aria-label='Comment']"));
-    child_comments = [];
-    for (var child_comment_div of top_level_li.querySelectorAll(
-            "div[aria-label='Comment reply']")) {
-      child_comments.push(process_comment(child_comment_div));
+  var top_level_ul =
+      document.getElementsByClassName(top_level_comments_class_name)[0];
+  for (var i = 0; i < top_level_ul.children.length; i++) {
+    var state = "initial";
+
+    var name = null;
+    var link = null;
+    var userid = null;
+    var ts = null;
+    var comment_html = null;
+
+    var parent_comment = null;
+    var child_comments = [];
+
+    var nodes = [top_level_ul.children[i]];
+    while (nodes.length > 0) {
+      var node = nodes.pop();
+
+      if (node.classList.contains(comment_class_name)) {
+        comment_html = node.innerHTML;
+      }
+
+      if (node.classList.contains(metadata_class_name)) {
+        link = node.getAttribute("href");
+        if (node.children.length == 1 && node.children[0].tagName == "ABBR") {
+          ts = node.children[0].getAttribute("data-utime");
+        }
+      }
+
+      if (node.classList.contains(author_class_name)) {
+        var data_hovercard = node.getAttribute("data-hovercard");
+        if (data_hovercard) {
+          var match = data_hovercard.match(
+            /.*[/]user[.]php[?]id=(?<userid>[0-9]+).*$/);
+          if (match && match.groups.userid) {
+            userid = match.groups.userid;
+          }
+        }
+
+        name = node.textContent;
+
+        // This is assuming author_class_name is the last one we get.
+
+        var comment = [name, link, userid, ts, comment_html];
+        name = null;
+        ts = null;
+        comment_html = null;
+        link = null;
+        userid = null;
+
+        if (parent_comment) {
+          child_comments.push(comment);
+        } else {
+          parent_comment = comment;
+        }
+      }
+      for (var j = 0; j < node.children.length; j++) {
+        nodes.push(node.children[j]);
+      }
     }
     collected_comments.push([parent_comment, child_comments]);
   }
@@ -81,10 +107,10 @@ function done(collected_comments) {
   document.title = "ready";
 }
 
-var expand_comments_button = document.querySelector(
-   "a[data-testid='UFI2CommentsCount/root']");
-if (expand_comments_button) {
-  expand_comments_button.click();
+var expand_comments_potential = document.getElementsByClassName(
+  open_comments_class_name);
+if (expand_comments_potential.length == 1) {
+  expand_comments_potential[0].click();
   expand_things(function() {
     var cta_button = document.querySelector(close_cta);
     if (cta_button) {
@@ -95,4 +121,3 @@ if (expand_comments_button) {
 } else {
   done([]);
 }
-
